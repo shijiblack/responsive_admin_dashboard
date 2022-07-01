d5d8f02d6db972613f5b9d11d1c6638fbf6b4c09
Shinji <shijiblack97@gmail.com>
2022-07-01 22:43:48 +0700

    Complete Project

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Oders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '87779',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lorem Hiand Kya',
+        productNumber: '87779',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Kim Home Joong',
+        productNumber: '87779',
+        paymentStatus: 'Due',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Foldable Mini Drone 09',
+        productNumber: '87779',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Mini Drone 587',
+        productNumber: '87779',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Mini Drone 78ye',
+        productNumber: '87779',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    }
+]
